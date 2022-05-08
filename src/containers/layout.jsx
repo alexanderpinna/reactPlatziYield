347b69e2c4e818c554d6d70aab1cb4b584808159
alex919090 <alex919090@gmail.com>
2022-05-08 19:00:48 -0400

    creacion de archivos

---
+++ src/containers/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const layouts = ({ children }) => {
+    return (
+        <div className="Layout">
+            {children}
+        </div>
+    );
+}
+
+export default layouts;
---
+++ src/routes/app.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from '../containers/layout';
+import Login from '../containers/login';
+import RecoveryPassword from '../containers/recoveryPassword';
+import Home from '../pages/home';
+import NotFound from '../pages/NotFound'
+import '../styles/global.css';
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/login" element={<Login />} />
+					<Route exact path="/recovery-password" element={<RecoveryPassword />} />
+					<Route path="*" element={<NotFound />}/>
+				</Routes>
+			</Layout>
+		</BrowserRouter>
+
+	);
+}
+
+
+
+export default App;
