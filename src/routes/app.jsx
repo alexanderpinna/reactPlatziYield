01f112e37119f6d014b2f726bf517924db07792c
alex919090 <alex919090@gmail.com>
2022-05-08 20:16:30 -0400

    creacion de Header

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from '../containers/layout';
 import Login from '../containers/login';
 import RecoveryPassword from '../containers/recoveryPassword';
-import Home from '../pages/home';
+import Home from '../pages/Home';
 import NotFound from '../pages/NotFound'
 import '../styles/global.css';
 
