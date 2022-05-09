19af4b71f662baa6ef9f2bc87c59002ada935da6
alex919090 <alex919090@gmail.com>
2022-05-09 11:26:55 -0400

    cambios por cambio de versiones

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './routes/app';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<App />, document.getElementById('app'));
+
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<App />);
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App tab="Home" />);
