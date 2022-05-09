eb36ae6b6f6e0fcab7bb0a8382d2196e6c9619d0
alex919090 <alex919090@gmail.com>
2022-05-09 11:31:57 -0400

    cambio de seccion en Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Header from '../components/Header'
+import ProductList from '../containers/ProductList'
 
 const home = () => {
     return (
-        <Header />
+        <ProductList />
     );
 }
 
