b48cf001bf9ae5b1d868514354d22f2607c2f88b
alex919090 <alex919090@gmail.com>
2022-05-09 10:31:39 -0400

    se agrego el import de los scss

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Menu';
+import '../styles/Menu.scss';
 
 const Menu = () => {
     return (
--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/OrderItem';
+import '../styles/OrderItem.scss';
 
 const ShoppingCartItem = () => {
     return (
